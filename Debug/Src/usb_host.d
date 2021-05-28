ffd2e755bcff4b4a42c6012e482584864ae2d72f
Keito Nakamori <akiresu133@gmail.com>
2021-05-28 21:21:40 +0900

    1st commit

---
+++ Debug/Src/usb_host.d
@@ -0,0 +1,125 @@
+Src/usb_host.o: ../Src/usb_host.c D:/STM/Practice02/Inc/usb_host.h \
+ D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h \
+ D:/STM/Practice02/Drivers/CMSIS/Include/core_cm4.h \
+ D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_version.h \
+ D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_compiler.h \
+ D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/STM/Practice02/Drivers/CMSIS/Include/mpu_armv7.h \
+ D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/STM/Practice02/Inc/stm32f4xx_hal_conf.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ D:/STM/Practice02/Inc/usbh_conf.h D:/STM/Practice02/Inc/main.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h \
+ D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc/usbh_cdc.h
+
+D:/STM/Practice02/Inc/usb_host.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_version.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Include/mpu_armv7.h:
+
+D:/STM/Practice02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/STM/Practice02/Inc/stm32f4xx_hal_conf.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+D:/STM/Practice02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+D:/STM/Practice02/Inc/usbh_conf.h:
+
+D:/STM/Practice02/Inc/main.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h:
+
+D:/STM/Practice02/Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc/usbh_cdc.h:
